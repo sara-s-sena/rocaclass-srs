779903d4a995b9bb39bc9987340a2ea1b18c9505
Sara Sena <sara.s.sena@outlook.com>
2026-04-13 03:13:17 -0300

    Phase 1: CARD TABLE SQLite created.

--- src/main.c
+++ src/main.c
@@ -13,6 +13,17 @@ int main() {
         return 1;
     }
 
+
+// SQL TABLE
+    char *errmsg = NULL;
+    
+    int exec_result = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT NOT NULL, phonetic TEXT, example TEXT, translation TEXT, easiness REAL DEFAULT 2.5, interval INTEGER DEFAULT 1, repetitions INTEGER DEFAULT 0, next_review TEXT DEFAULT (date('now')));", NULL, NULL, &errmsg);
+    if (exec_result == SQLITE_OK) {
+        printf("Table created.");
+    } else {
+        printf("Failled to create table.");
+    }
+
     sqlite3_close(db);
 
     return 0;
