5905de06bdd9ac3ec709c7f1696ee46e42852028
Sara Sena <sara.s.sena@outlook.com>
2026-05-03 01:08:56 -0300

    Phase 3: implement --add and clean up debug output

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "card.h"
 #include <string.h>
 #include "sm2.h"
-
+#include <time.h>
 
 
 // the interactive review loop
@@ -14,50 +14,176 @@ int list_cards(sqlite3 *db) {
     sqlite3_stmt *stmt = NULL;
 
 
-    int return_value_SELECT = sqlite3_prepare_v2(db, "SELECT * FROM cards", -1, &stmt, NULL);
-    if(return_value_SELECT != SQLITE_OK) {
-        printf("SQL error: %s\n", sqlite3_errmsg(db));
-        return 1;
-    }
+int return_value_SELECT = sqlite3_prepare_v2(db, "SELECT * FROM cards", -1, &stmt, NULL);
+if(return_value_SELECT != SQLITE_OK) {
+    printf("SQL error: %s\n", sqlite3_errmsg(db));
+    return 1;
+}
 
-    while(sqlite3_step(stmt) == SQLITE_ROW) {
+while(sqlite3_step(stmt) == SQLITE_ROW) {
 
 // SQLITE SELECT / sqlite3_column_*()
-        const unsigned char *word = sqlite3_column_text(stmt, 1);
-        printf("word: %s\n", word);
+    const unsigned char *word = sqlite3_column_text(stmt, 1);
+    printf("word: %s\n", word);
 
-        const unsigned char *translation = sqlite3_column_text(stmt, 2);
-        printf("translation: %s\n", translation);
+    const unsigned char *translation = sqlite3_column_text(stmt, 2);
+    printf("translation: %s\n", translation);
 
-        const unsigned char *phonetic = sqlite3_column_text(stmt, 3);
-        printf("phonetic: %s\n", phonetic);
+    const unsigned char *phonetic = sqlite3_column_text(stmt, 3);
+    printf("phonetic: %s\n", phonetic);
 
-        const unsigned char *example = sqlite3_column_text(stmt, 4);
-        printf("example: %s\n", example);
+    const unsigned char *example = sqlite3_column_text(stmt, 4);
+    printf("example: %s\n", example);
 
-        const unsigned char *explanation = sqlite3_column_text(stmt, 5);
-        printf("explanation: %s\n", explanation);
+    const unsigned char *explanation = sqlite3_column_text(stmt, 5);
+    printf("explanation: %s\n", explanation);
 
-        double easiness = sqlite3_column_double(stmt, 6);
-        printf("easiness: %f\n", easiness);
+    double easiness = sqlite3_column_double(stmt, 6);
+    printf("easiness: %f\n", easiness);
 
-        int interval = sqlite3_column_int(stmt, 7);
-        printf("interval: %d\n", interval);
+    int interval = sqlite3_column_int(stmt, 7);
+    printf("interval: %d\n", interval);
 
-        int repetitions = sqlite3_column_int(stmt, 8);
-        printf("repetitions: %d\n", repetitions);
+    int repetitions = sqlite3_column_int(stmt, 8);
+    printf("repetitions: %d\n", repetitions);
 
-        const unsigned char *next_review = sqlite3_column_text(stmt, 9);
-        printf("next_review: %s\n", next_review);
+    const unsigned char *next_review = sqlite3_column_text(stmt, 9);
+    printf("next_review: %s\n", next_review);
     }
 
-    int return_value_finalize_select = sqlite3_finalize(stmt);
-    if(return_value_finalize_select == SQLITE_OK) {
-        printf("return_value_finalize_select OK.\n");
-    }   else {
-        printf("return_value_finalize_select ERROR.\n");
+int return_value_finalize_select = sqlite3_finalize(stmt);
+if(return_value_finalize_select != SQLITE_OK) {
+    printf("return_value_finalize_select ERROR: %s\n", sqlite3_errmsg(db));
+    return 1;
+}
+
+    return 0;
+}
+
+int add_card(sqlite3 *db) {
+    printf("Enter word: \n");
+    char word[128];
+    fgets(word, sizeof(word), stdin);
+    word[strcspn(word, "\n")] = '\0';
+    printf("Enter translation: \n");
+    char translation[256];
+    fgets(translation, sizeof(translation), stdin);
+    translation[strcspn(translation, "\n")] = '\0';
+    printf("Enter phonetic: \n");
+    char phonetic[128];
+    fgets(phonetic, sizeof(phonetic), stdin);
+    phonetic[strcspn(phonetic, "\n")] = '\0';
+    printf("Enter example: \n");
+    char example[512];
+    fgets(example, sizeof(example), stdin);
+    example[strcspn(example, "\n")] = '\0';
+    printf("Enter explanation: \n");
+    char explanation[512];
+    fgets(explanation, sizeof(explanation), stdin);
+    explanation[strcspn(explanation, "\n")] = '\0';
+
+    Card card;
+    strcpy(card.word, word);
+    strcpy(card.translation, translation);
+    strcpy(card.phonetic, phonetic);
+    strcpy(card.example, example);
+    strcpy(card.explanation, explanation);
+    card.easiness = 2.5;
+    card.interval = 1;
+    card.repetitions = 0;
+    time_t t = time(NULL);
+    struct tm *now = localtime(&t);
+
+    char buffer[11]; // YYYY-MM-DD
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", now);
+    strcpy(card.next_review, buffer);
+
+    // SQLITE3 STATEMENT
+
+    sqlite3_stmt *stmt = NULL;
+        
+    int return_value_stmt = sqlite3_prepare_v2(db, "INSERT INTO cards (word, translation, phonetic, example, "
+    "explanation, easiness, interval, repetitions, next_review) "
+    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", -1, &stmt, NULL);
+
+    if(return_value_stmt != SQLITE_OK) {
+        printf("sqlite3_prepare_v2 ERROR: %s\n", sqlite3_errmsg(db));
         return 1;
     }
+
+    // SQLITE3 BIND
+
+    int return_value_bind_word = sqlite3_bind_text(stmt, 1, card.word, -1, SQLITE_STATIC);
+    if(return_value_bind_word != SQLITE_OK) {
+        printf("return_value_bind_word ERROR\n");
+        return 1;
+    }
+
+    int return_value_bind_translation = (sqlite3_bind_text(stmt, 2, card.translation, -1, SQLITE_STATIC));
+    if(return_value_bind_translation != SQLITE_OK) {
+        printf("return_value_bind_translation ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_phonetic = (sqlite3_bind_text(stmt, 3, card.phonetic, -1, SQLITE_STATIC));
+    if(return_value_bind_phonetic != SQLITE_OK) {
+        printf("return_value_bind_phonetic ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_example = (sqlite3_bind_text(stmt, 4, card.example, -1, SQLITE_STATIC));
+    if(return_value_bind_example != SQLITE_OK) {
+        printf("return_value_bind_example ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_explanation = (sqlite3_bind_text(stmt, 5, card.explanation, -1, SQLITE_STATIC));
+    if(return_value_bind_explanation != SQLITE_OK) {
+        printf("return_value_bind_explanation ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_easiness = (sqlite3_bind_double(stmt, 6, card.easiness));
+    if(return_value_bind_easiness != SQLITE_OK) {
+        printf("return_value_bind_easiness ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_interval = (sqlite3_bind_int(stmt, 7, card.interval));
+    if(return_value_bind_interval != SQLITE_OK) {
+        printf("return_value_bind_interval ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_repetitions = (sqlite3_bind_int(stmt, 8, card.repetitions));
+    if(return_value_bind_repetitions != SQLITE_OK) {
+        printf("return_value_bind_repetitions ERROR.\n");
+        return 1;
+    }
+
+    int return_value_bind_next_review = (sqlite3_bind_text(stmt, 9, card.next_review, -1, SQLITE_STATIC));
+    if(return_value_bind_next_review != SQLITE_OK) {
+        printf("return_value_next_review ERROR.\n");
+        return 1;
+    }
+
+// SQLITE3 STEP
+
+    int return_value_step = sqlite3_step(stmt);
+    if(return_value_step != SQLITE_DONE) {
+        printf("return_value_step ERROR: %s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+
+// SQLITE FINALIZE
+
+    int return_value_finalize = sqlite3_finalize(stmt);
+    if(return_value_finalize != SQLITE_OK) {
+        printf("return_value_finalize ERROR: %s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    printf("Card added successfully.\n");
 
     return 0;
 }
@@ -76,11 +202,8 @@ int main(int argc, char *argv[]) {
     sqlite3* db;
     int result = sqlite3_open("rocaclass.db", &db);
 
-    if (result == SQLITE_OK) {
-        printf("The connection opened successfully.\n");
-    } else {
-        const char* errorMessage = sqlite3_errmsg(db);
-        printf("Failed to open database: %s\n", errorMessage);
+    if(result != SQLITE_OK) {
+        printf("sqlite3_open: %s\n", sqlite3_errmsg(db));
         return 1;
     }
 
@@ -100,18 +223,18 @@ int main(int argc, char *argv[]) {
         "interval INTEGER DEFAULT 1, "
         "repetitions INTEGER DEFAULT 0, "
         "next_review TEXT DEFAULT (date('now')));", NULL, NULL, &errmsg);
-    if (exec_result == SQLITE_OK) {
-        printf("Table created.\n");
-    }   else {
-            printf("Failed to create table: %s\n", errmsg);
-            sqlite3_free(errmsg);
-            return 1;
+
+    if(exec_result != SQLITE_OK) {
+        printf("exec_result: %s\n", sqlite3_errmsg(db));
+        sqlite3_free(errmsg);
+        return 1;
     }
+
 
     if (strcmp(argv[1], "--list") == 0) {
         list_cards(db);        
     } else if (strcmp(argv[1], "--add") == 0) {
-        printf("--add not yet implemented.\n");
+        add_card(db);
     }   else {
             printf("invalid command. Usage: %s [--list | --add | --review]\n", argv[0]);
     }
